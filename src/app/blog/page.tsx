ab8ffdaba052e4d7554f52d681b3fc3b8cb06504
rick-love <ricklove@outlook.com>
2023-12-03 01:03:16 +0100

    First

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export default function Page() {
+  return (
+    <div>
+      <h1>BLOG Route</h1>
+      <h2>
+        <Link href="/">Home</Link>
+      </h2>
+
+    </div>
+  )
+}
